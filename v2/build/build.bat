82ae8dd78386e7894a0235427e4a1ba34b237c8b
Carson <jackparadise520a@gmail.com>
2022-06-07 16:41:48 +0800

    Update build.bat (-ldflags "-s -w") (#5)

--- v2/build/build.bat
+++ v2/build/build.bat
@@ -1,7 +1,10 @@
 @echo off
+cd ..
 go env -w CC=C:\mingw64\bin\gcc
 go env -w CXX=C:\mingw64\bin\g++
-go build
+go build -ldflags "-s -w" -o ..\bin\go-controller.exe
+:: move go-controller.exe ..\bin
+start ..\bin
 echo restore & echo.
 go env -w CC=gcc
 go env -w CXX=g++
